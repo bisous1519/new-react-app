90485aa72ca64f3f1e82f3d75b6789a0c9324af4
bisous1519 <anipap2@naver.com>
2020-11-21 23:37:29 +0900

    modern-react1

---
+++ vlpt-test/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Counter from "./components/Counter";
+import Hello from "./components/Hello";
+
+function App() {
+  return (
+    <>
+      <Hello name="엄지" color="royalblue" />
+      <Hello name="엄지" color="royalblue" />
+    </>
+  );
+}
+
+export default App;
---
+++ vlpt-test/src/components/Hello.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Hello({ name, color }) {
+  return <div style={{ color }}>{name}, 안녕!</div>;
+}
